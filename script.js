e9dd2e2023ee55544329fa9917011b035d7aa39d
David Arhin <davidarhin2005@gmail.com>
2023-10-22 19:27:10 +0000

    reponsiveness

--- script.js
+++ script.js
@@ -18,3 +18,59 @@ function addAnimation() {
         });
     });
 }
+
+
+// ------------Search Codes---------
+
+// Sample data for searching
+const data = [
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Date",
+    "Elderberry",
+    "Fig",
+    "Grapes",
+    "Honeydew",
+    "Kiwi",
+    "Lemon",
+    "Mango",
+    "Orange",
+    "Peach",
+];
+
+// Function to perform the search
+function performSearch(query) {
+    const results = data.filter(item => item.toLowerCase().includes(query.toLowerCase()));
+    displayResults(results);
+}
+
+// Function to display search results on the page
+function displayResults(results) {
+    const resultsContainer = document.getElementById("searchResults");
+    resultsContainer.innerHTML = "";
+
+    if (results.length === 0) {
+        resultsContainer.innerHTML = "No results found.";
+    } else {
+        results.forEach(result => {
+            const resultElement = document.createElement("div");
+            resultElement.textContent = result;
+            resultsContainer.appendChild(resultElement);
+        });
+    }
+}
+
+// Event listeners for search input and button
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", () => {
+    performSearch(searchInput.value);
+});
+
+searchInput.addEventListener("keyup", event => {
+    if (event.key === "Enter") {
+        performSearch(searchInput.value);
+    }
+});
