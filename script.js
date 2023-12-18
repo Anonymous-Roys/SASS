70461a7dfbfe3317ae5a7bc9629c5c0268920537
David Arhin <davidarhin2005@gmail.com>
2023-12-18 22:20:29 +0000

    scripts

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+// let words = document.querySelectorAll(".word");
+// words.forEach((word) =>{
+//     let letters = word.textContent.split("");
+//     word.textContent = "";
+//     letters.forEach((letter) =>{
+//         let span = document.createElement("span");
+//         span.textContent = letter;
+//         span.className = "letter";
+//         word.append(span);
+//     });
+// });
+
+// let currentWordIndex = 0;
+// let maxWordIndex = words.length -1;
+// words[currentWordIndex].style.opacity ="1";
+
+// let changeText = () =>{
+//     let currentWord = words[currentWordIndex];
+//     let nextWord = currentWordIndex === nextWordIndex ? words[0] : words[currentWordIndex + 1];
+
+//     Array.from(currentWord.children).forEach((letter, i) =>{
+//         setTimeout(() =>{
+//             letter.className = "letter out";
+
+//         }, i *100)
+//     });
+//     nextWord.style.opacity="1";
+
+//     Array.from(nextWord.children).forEach((letter, i)=>{
+//         letter.className ="letter behind";
+//         setTimeout(() =>{
+//             letter.className = "letter in";
+//         }, 340 + i * 80);
+//     });
+
+//     currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+// }
+
+// changeText();
+// setInterval(changeText, 3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const scrollers = document.querySelectorAll(".content-container");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
